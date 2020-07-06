425f23f997403422cdf027d9bb503503ca4a9101
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-07-05 19:30:52 -0500

    day 2 javascript course

--- notes.js
+++ notes.js
@@ -21,3 +21,32 @@ console.log(bool);
 console.log('I know my body mass indexes was all types of messed up but John has a lower bmi than Mark and thats' + ' ' + bool);
 
 // on that note i'm done with Day 1 of this javascript course so i'm gonna call it a night. gg
+
+
+// DAY 2    
+
+var johnsAvg, mikesAvg, maryAvg;
+
+johnsAvg = (89 + 120 + 103) / 3;
+mikesAvg = (116 + 94 + 123) / 3;
+maryAvg = (97 + 134 + 105) / 3;
+
+if (johnsAvg > mikesAvg && maryAvg) {
+
+    console.log('John\'s team hide the highest avg at' + ' ' + johnsAvg);
+} 
+
+    else if (maryAvg > johnsAvg && mikesAvg) {
+    console.log('Mary\'s team hide the highest avg at' + ' ' +  maryAvg);
+}
+
+    else if (johnsAvg === mikesAvg && maryAvg) {
+        console.log('both teams had the same avg at' + ' ' +  johnsAvg);
+}
+
+    else {
+
+    console.log('Mike\'s team hide the highest avg at' + ' ' +  mikesAvg);
+}
+
+console.log(maryAvg, mikesAvg, johnsAvg);
