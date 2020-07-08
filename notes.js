efab88c98ce4ae4884e3c0ae3c3d111da8623fdb
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-07-08 16:23:44 -0500

    js course day 5

--- notes.js
+++ notes.js
@@ -172,11 +172,107 @@ john.bmi();
 
 if (john.bmi > mark.bmi) {
     console.log(john.name + ' has a higher bmi than' + ' ' + mark.name + ' at a BMI of ' + john.bmi)
-} else {
+} 
+    else if (john.bmi === mark.bmi) {
+        console.log('Both ' + john.name + ' & ' + mark.name + ' got the same bmi at ' + john.bmi)
+    }
+
+    else {
     console.log(john.name + ' go getcha ass in the gym right now because ' + mark.name + ' has a higher bmi than you at ' + mark.bmi +'.')
 }
 
+// day 5 take 1
 
 
+const restaurantBills = [42, 48, 124, 180, 268]
+const restaurantTips = []
+const restaurantTotalBill = []
+
+ for (let i = 0; i < restaurantBills.length; i++) {
+        if (restaurantBills[i] <= 50) {
+            restaurantTips.push(restaurantBills[i]*.2);
+            restaurantTotalBill.push((restaurantBills[i]*.2) + restaurantBills[i]);
+        }
+
+        else if (restaurantBills[i] > 50 && restaurantBills[i] < 200) {
+            restaurantTips.push(restaurantBills[i]*.15);
+            restaurantTotalBill.push((restaurantBills[i]*.15) + restaurantBills[i]);
+        }
+
+        else if (restaurantBills[i] >= 200) {
+            restaurantTips.push(restaurantBills[i]*.10);
+            restaurantTotalBill.push((restaurantBills[i]*.1) + restaurantBills[i]);
+        }
+
+   // restaurantTotalBill.push(restaurantBills[i])
+} 
+
+console.log(restaurantBills);
+console.log(restaurantTips);
+console.log(restaurantTotalBill);
+
+const marksRestaurantBills = [45, 77, 110, 375]
+const marksRestaurantTips = []
+
+for (let i = 0; i < marksRestaurantBills.length; i++) {
+    if (marksRestaurantBills[i] <= 50) {
+        marksRestaurantTips.push(marksRestaurantBills[i]*.2);
+        
+    }
+
+    else if (marksRestaurantBills[i] > 50 && marksRestaurantBills[i] < 200) {
+        marksRestaurantTips.push(marksRestaurantBills[i]*.15);
+        
+    }
+
+    else if (marksRestaurantBills[i] >= 200) {
+        marksRestaurantTips.push(marksRestaurantBills[i]*.10);
+        
+    }
+}
+
+console.log('RESPECTTT');
+
+console.log(marksRestaurantTips);
+
+console.log('RESPECTTT');
+/*
+let totalTips = 0;
+
+for (let i of marksRestaurantTips) {
+    totalTips += i
+    
+}
 
 
+let avg = totalTips / marksRestaurantTips.length
+
+console.log(totalTips)
+
+console.log(avg);
+
+*/
+
+const markTotalTips = marksRestaurantTips.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+});
+
+console.log(markTotalTips);
+
+const markAvgTips = markTotalTips/marksRestaurantTips.length;
+console.log(markAvgTips);
+
+const johnTotalTips = restaurantTips.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+});
+
+console.log(johnTotalTips);
+
+const johnAvgTips = johnTotalTips/ restaurantTips.length;
+console.log(johnAvgTips);
+
+if (johnAvgTips > markAvgTips) {
+    console.log(mark.name + ' you cheap son of a bitch you couldn\'t afford to tip ' + (johnAvgTips - markAvgTips) + ' more cents to keep up with John? I can\'t eat out with you anymore, and I want a divorce.')
+} else {
+    console.log(john.name + ' you cheap son of a bitch you couldn\'t afford to tip ' + (markAvgTips - johnAvgTips) + ' more cents to keep up with Mark? I can\'t eat out with you anymore, and  I want a divorce')
+}
